eaf269b3dee15a91bdcf6f9dd581a5e8a1276275
devesh-verma <deveshverma619@gmail.com>
2019-11-19 11:09:47 +0530

    added auth related functionality to auth.js

---
+++ app/routes/auth.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/auth');
+
+// user login
+router.post(
+    '/login',
+    authController.authenticateUser
+);
+
+// user logout
+router.post(
+    '/logout',
+    authController.logout
+)
+
+// logout all sessions
+router.post(
+    '/logoutAll', 
+    authController.logoutAll
+)
+
+module.exports = router;
--- app/routes/user.js
+++ app/routes/user.js
@@ -3,24 +3,27 @@ const router = express.Router();
 const userController = require('../controllers/user');
 
 // create a user
-router.post('/', userController.createUser);
-
-// user login
-router.post('/login', userController.authenticateUser);
-
-// user logout
-router.post('/logout', userController.logout)
-
-// logout all sessions
-router.post('/logoutAll', userController.logoutAll)
+router.post(
+    '/',
+    userController.createUser
+);
 
 // get user profile
-router.get('/me', userController.userProfile)
+router.get(
+    '/me',
+    userController.userProfile
+);
 
 // update user info
-router.patch('/me', userController.userProfileUpdate)
+router.patch(
+    '/me',
+    userController.userProfileUpdate
+);
 
 // delete a user
-router.delete('/me', userController.userDelete)
+router.delete(
+    '/me',
+    userController.userDelete
+);
 
 module.exports = router;
