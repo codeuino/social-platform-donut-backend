8be21d3539ee8232199fc52e4eddefa1ac76aca7
devesh-verma <deveshverma619@gmail.com>
2019-12-01 13:03:41 +0530

    hardcoded documentaion link in index file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var router = express.Router()
-
+const documentation_url = "https://documenter.getpostman.com/view/1159934/SWDze1Rp"
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+  res.redirect(documentation_url);
 })
 
 module.exports = router
