40b742d5cf176121fa6daffe3d08b5b58397ffe2
devesh-verma <deveshverma619@gmail.com>
2019-12-01 15:30:42 +0530

    adds user - fetch,update,delete functionality

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/User')
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.header('Authorization').replace('Bearer', '')
+    const token = req.header('Authorization').replace('Bearer ', '')
     const decoded = jwt.verify(token, process.env.JWT_SECRET)
     const user = await User.findOne({
       _id: decoded._id,
--- test/user.test.js
+++ test/user.test.js
@@ -20,6 +20,17 @@ const demoUser = {
   password: 'demoUser@123'
 }
 
+let server
+/**
+ * This will pe performed once at the begining of the test
+ */
+beforeAll(async (done) => {
+  server = app.listen(4000, () => {
+    global.agent = request.agent(server)
+    done()
+  })
+})
+
 /**
  * This deletes all the existing user in database,
  * and creates a new user in database with the provided details.
@@ -60,13 +71,74 @@ test('Should signup new user', async () => {
   expect(user.password).not.toBe('devesh@123')
 })
 
+/** Testing user login */
+test('Login existing user', async () => {
+  const response = await request(app)
+    .post('/auth/login')
+    .send({
+      email: testUser.email,
+      password: testUser.password
+    })
+    .expect(200)
+
+  const user = await User.findById(testUserId)
+  expect(response.body.token).toBe(user.tokens[1].token)
+})
+
+/** Testing non existing user login */
+test('Should not login non-existing user', async () => {
+  await request(app).post('/auth/login').send({
+    email: 'random@random.com',
+    password: 'random@123'
+  }).expect(400)
+})
+
+/** Fetch authenticated user profile */
+test('Should get profile for user', async () => {
+  await request(app)
+    .get('/user/me')
+    .set('Authorization', `Bearer ${testUser.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+/** Fail in getting unathenticated user profile */
+test('Should not get profile for unauthenticated user', async () => {
+  await request(app)
+    .get('/user/me')
+    .send()
+    .expect(401)
+})
+
+/** Delete authenticated user profile */
+test('Should delete profile of authenticated user', async () => {
+  await request(app)
+    .delete('/user/me')
+    .set('Authorization', `Bearer ${testUser.tokens[0].token}`)
+    .send()
+    .expect(200)
+
+  // Assert that user was deleted
+  const user = await User.findById(testUserId)
+  expect(user).toBeNull()
+})
+
+/** Fail deleting user profile of unauthenticated user */
+test('Should not delete profile of unauthenticated user', async () => {
+  await request(app)
+    .delete('/user/me')
+    .send()
+    .expect(401)
+})
+
 /**
  * TODO: FIX ERROR
  * This is a temporary fix to issue:
  * Jest has detected the following 1 open handle potentially keeping Jest from exiting
  */
-afterAll(done => {
+afterAll(async () => {
+  // close server
+  await server.close()
   // Closing the DB connection allows Jest to exit successfully.
-  mongoose.connection.close()
-  done()
+  await mongoose.connection.close()
 })
