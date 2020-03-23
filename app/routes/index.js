f04cbce8fd82414f896917858ea46415269c7679
Devansh Agarwal <devanshagarwal50@gmail.com>
2020-03-23 15:43:03 +0530

    Changed the required names

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,25 +1,14 @@
 var express = require('express')
 var router = express.Router()
 const documentationUrl = 'https://documenter.getpostman.com/view/1159934/SWDze1Rp'
-const Url = require('../models/ShortUrl');
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.redirect(documentationUrl)
 })
 
-router.get('/:shorturl',async (req,res)=>{
-  try {
-    const url = await Url.findOne({urlcode: req.params.shorturl});
-    
-    if (url) {
-      return res.redirect(url.longurl);
-    }
-    else{
-      return  res.json('No url found!');
-    }
-  } catch (error) {
-    res.json('Server error!')
-  }
-});
+router.get('/:shorturl', (req,res,next)=>{
+  res.redirect('/shortUrl/'+ req.params.shorturl);
+})
 
 module.exports = router
