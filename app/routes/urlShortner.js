da7b2ba462eb3063f33cf0859c49b261ac6c6862
Devansh Agarwal <devanshagarwal50@gmail.com>
2020-03-25 13:12:32 +0530

    Formatted the code

--- app/routes/urlShortner.js
+++ app/routes/urlShortner.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const router = express.Router()
-const shortnerController = require('../controllers/UrlShortner');
+const shortnerController = require('../controllers/UrlShortner')
 
-//Redirects the ShortURL back to LongURL
+// Redirects the ShortURL back to LongURL
 router.get(
   '/:shorturl',
   shortnerController.redirect
-  );
+)
 
-//Shorten the LongURL and saves in DB
+// Shorten the LongURL and saves in DB
 router.post(
   '/shorten',
   shortnerController.shorten
-  );
+)
 
 module.exports = router
