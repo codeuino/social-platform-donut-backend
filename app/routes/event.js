0a6f20034a294057c309531c659227210669ec77
Rupesh Krishna Jha <rupeshiya@gmail.com>
2020-05-28 15:55:10 +0530

    working mechanism for triggering maintenace, updating org settings by admin, pin post

--- app/routes/event.js
+++ app/routes/event.js
@@ -2,40 +2,47 @@ const express = require('express')
 const auth = require('../middleware/auth')
 const router = express.Router()
 const eventController = require('../controllers/event')
+const isUnderMaintenance = require('../middleware/maintenance')
 
 // get all the events
 router.get(
   '/all',
+  isUnderMaintenance,
   auth,
   eventController.GetAllEvent
 )
 // create an event
 router.post(
   '/',
+  isUnderMaintenance,
   auth,
   eventController.createEvent
 )
 // get event by id
 router.get(
   '/:id',
+  isUnderMaintenance,
   auth,
   eventController.GetEventById
 )
 // update an event
 router.patch(
   '/:id',
+  isUnderMaintenance,
   auth,
   eventController.updateEvent
 )
 // rsvp by user
 router.post(
   '/rsvp/:id',
+  isUnderMaintenance,
   auth,
   eventController.rsvp
 )
 // delete an event
 router.delete(
   '/:id',
+  isUnderMaintenance,
   auth,
   eventController.deleteEvent
 )
