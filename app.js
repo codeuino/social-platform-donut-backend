86461c826ddedaf15409a7e88d43ac1f65757214
K mehant <411843@student.nitandhra.ac.in>
2020-07-09 23:14:54 +0530

    Bypass clickjacking attack
    Signed-off-by: K mehant <411843@student.nitandhra.ac.in>

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 app.use((req, res, next) => {
+  res.append('X-Frame-Options', 'SAMEORIGIN')
   req.io = io
   next()
 })
