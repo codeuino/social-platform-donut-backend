7a1b6e43ab09305eec775603612429378e7db379
lazycipher <17141301+lazycipher@users.noreply.github.com>
2020-11-30 16:07:27 +0530

    handle if no file attached

--- app/utils/compressAndUpload.js
+++ app/utils/compressAndUpload.js
@@ -5,13 +5,26 @@ const MAX_WIDTH = 768
 
 const imageCompressor = (req, res, next) => {
   try {
-    Jimp.read(req.file.buffer, (err, image) => {
-      if (err) throw err;
+    if(req.file && req.file.buffer){
+      Jimp.read(req.file.buffer, (err, image) => {
+        if (err) throw err;
 
-      if (image.bitmap.width > MAX_WIDTH) {
-        image.resize(768, Jimp.AUTO)
-          .getBuffer(Jimp.AUTO, (err, image) => {
-            if (err) throw err
+        if (image.bitmap.width > MAX_WIDTH) {
+          image.resize(768, Jimp.AUTO)
+            .getBuffer(Jimp.AUTO, (err, image) => {
+              if (err) throw err
+
+              uploader.uploadToAWS(image, req.file, (err, data) => {
+                if (err) throw err
+
+                req.file.href = data.Location
+                req.file.key = data.Key
+                next(null, true);
+              })
+            })
+        } else {
+          image.getBuffer(Jimp.AUTO, (err, image) => {
+            if (err) throw err;
 
             uploader.uploadToAWS(image, req.file, (err, data) => {
               if (err) throw err
@@ -21,22 +34,13 @@ const imageCompressor = (req, res, next) => {
               next(null, true);
             })
           })
-      } else {
-        image.getBuffer(Jimp.AUTO, (err, image) => {
-          if (err) throw err;
-
-          uploader.uploadToAWS(image, req.file, (err, data) => {
-            if (err) throw err
-
-            req.file.href = data.Location
-            req.file.key = data.Key
-            next(null, true);
-          })
-        })
-      }
-    })
+        }
+      })
+    } else {
+      next()
+    }
   } catch(error) {
-    next(err, false)
+    next(error, false)
   }
 }
 
